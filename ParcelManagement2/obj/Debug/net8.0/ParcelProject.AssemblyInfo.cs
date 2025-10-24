2637345d5a3e5fd0924cf2271ce23625c0b942a6
a21681153 <a21681153@gmail.com>
2025-10-25 00:55:33 +0800

    頁面顏色-薄荷青

--- ParcelManagement2/obj/Debug/net8.0/ParcelProject.AssemblyInfo.cs
+++ ParcelManagement2/obj/Debug/net8.0/ParcelProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParcelProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d80fe9e6702be3db1033146ccd14b88cda3e40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6921a914347f73e4e4f33ca9e495050642db50bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParcelProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParcelProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
